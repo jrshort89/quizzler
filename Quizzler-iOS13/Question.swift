0a8be86a54b9561fb188af940d3de586a0cc632f
jrshort89 <jrshort89@gmail.com>
2021-03-31 08:44:13 -0400

    questions work

--- Quizzler-iOS13/Question.swift
+++ Quizzler-iOS13/Question.swift
@@ -11,4 +11,9 @@ import Foundation
 struct Question {
     let text: String
     let answer: Bool
+    
+    init(q: String, a: Bool) {
+        text = q
+        answer = a
+    }
 }
--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -11,25 +11,55 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var labelText: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
     
     var questionNumber = 0
+    var correctAnswer = 0
     
     let quiz = [
-        Question(text: "Am I dope?", answer: true),
-        Question(text: "Are you dope?", answer: false)
+        Question(q: "Am I dope?", a: true),
+        Question(q: "Are you dope?", a: false)
     ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         labelText.text = quiz[questionNumber].text
+        progressBar.progress = Float(questionNumber) / Float(quiz.count)
     }
 
     @IBAction func buttonHandler(_ sender: UIButton) {
         
+        if sender.currentTitle == "True" && quiz[questionNumber].answer {
+            correctAnswer += 1
+            sender.backgroundColor = UIColor.green
+        } else if sender.currentTitle == "False" && !quiz[questionNumber].answer {
+            correctAnswer += 1
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+        
+        if questionNumber < quiz.count - 1 {
+            questionNumber += 1
+            progressBar.progress = Float(questionNumber) / Float(quiz.count)
+            labelText.text = quiz[questionNumber].text
+        } else {
+            progressBar.progress = 1
+            labelText.text = "\(String((Float(correctAnswer) / Float(quiz.count) * 100)))%"
+        }
+        
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(resetButtonColor), userInfo: nil, repeats: false)
+        
+              
+        
     }
     
-    
+    @objc func resetButtonColor() {
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+    }
     
 }
 
